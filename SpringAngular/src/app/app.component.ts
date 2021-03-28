245728f7f11db3dc92ff4751f17c73d8c007557d
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-03-28 23:56:44 +0200

    second

--- SpringAngular/src/app/app.component.ts
+++ SpringAngular/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeesService } from './employees.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'SpringAngular';
+export class AppComponent implements OnInit {
+  public employees!: Employee[];
+
+  constructor(private  employeesService: EmployeesService) { }
+
+  ngOnInit(){
+    this.getEmployees
+  }
+  public getEmployees(): void {
+     this.employeesService.getEmployees().subscribe(
+       (responce: Employee[]) => {
+         this.employees = responce;
+        },
+        (error: HttpErrorResponse) =>{
+          alert(error.message);
+        }
+        
+     );
+  }
+
 }
