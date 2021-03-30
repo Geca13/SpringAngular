2457daa362d89a176f74edd81dbb5039d9361ddf
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-03-30 08:27:03 +0200

    edit

--- SpringAngular/src/app/app.component.ts
+++ SpringAngular/src/app/app.component.ts
@@ -11,6 +11,7 @@ import { EmployeeService } from './employee.service';
 })
 export class AppComponent implements OnInit {
   public employees!: Employee[];
+  public editEmployee!: Employee;
 
   constructor(private employeeService: EmployeeService) { }
 
@@ -27,6 +28,19 @@ export class AppComponent implements OnInit {
         }
         
      );
+  }
+
+  public onUpdateEmployee(employee: Employee):void{
+    
+    this.employeeService.updateEmployee(employee).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
   }
 
   public onAddEmployee(addForm: NgForm):void{
@@ -52,6 +66,7 @@ export class AppComponent implements OnInit {
       button.setAttribute('data-target','#addEmployeeModal');
     }
     if(mode === 'edit'){
+      this.editEmployee=employee;
       button.setAttribute('data-target','#updateEmployeeModal');
     }
     if(mode === 'delete'){
